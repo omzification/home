e74cfde3a561aefa1da76894642a8e8aba6f6d80
omzification <115732406+omzification@users.noreply.github.com>
2023-02-09 21:01:02 +0000

    abc

--- index.php
+++ index.php
@@ -44,17 +44,13 @@
     <meta name="google-signin-client_id" content="<154362449771-cnveshrlrbn36aq94meu9ie254uc9gku.apps.googleusercontent.com>">
     <meta name="google-signin-scope" content="https://www.googleapis.com/auth/analytics.readonly">
     <!-- <script type="text/javascript" src="newrelic.browser.js"></script> -->
-    <link rel="manifest" href="/manifest.json">
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
--- slaptop/slaptop.js
+++ slaptop/slaptop.js
@@ -26,3 +26,110 @@ setupNav = function () {
 }
 
 
+setupFlickr = function() {
+
+
+
+
+
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+$.getJSON(flickerAPI, {
+id: "197155581@N04",
+tags: "test, test2",
+tagmode: "any",
+format: "json"
+})
+.done(function (result, status, xhr) {
+
+console.log("Connecting to Flickr: " + status);
+
+const picsData = result.items;
+
+console.log('Flickr pics: ');
+console.log(picsData);
+
+
+if (status == "success") {
+  picsFolder(picsData)
+}
+
+}).fail(function (xhr, status, error) {
+console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
+$('.picsFolderContent').append('<span> oops. something seems to have gone wrong :/ </span>');
+});
+
+
+// loading 'pics' folder with sub-folders using photo tags
+
+function picsFolder(picsData) {
+
+picsTag = picsData.map(item => item.tags)
+.filter((value, index, self) => self.indexOf(value) === index);
+
+console.log("Picture Tags: " + picsTag);
+
+for (let i = 0; i < picsTag.length; i++) {
+
+const tag = picsTag[i];
+
+tagData = picsData.filter(tagGroup => tagGroup.tags == picsTag[i]);
+
+$('.picsFolderContent')
+  .append('<div id="'
+       + tag +
+       '-folder" class="folder-icon"><div><img src="/slaptop/assets/icons/Folder.png"></div><span>'
+       + tag +
+       '</span></div>');
+
+loadTagFolders(tagData, tag);
+
+openTagFolder(tag);
+
+
+
+}
+
+}
+
+function loadTagFolders(tagData, tag) {
+
+var $tagsDialogs = $('<div></div>')
+  .addClass("tagsFolderContent " + tag + "")
+  .dialog({
+      autoOpen: false,
+      title: tag,
+      resizable: false,
+  });
+
+$.each(tagData, function (i, pic) {
+
+
+
+
+
+$("<div id="
++ pic.title +
+" class=" + 'card-pic' + "><div><img id="
+   + pic.title +
+  "-src src=" + pic.media.m + "></div><span>"
+   + pic.title +
+   "</span></div>").appendTo("." + tag + "");
+});
+console.log(tagData);
+
+}
+
+
+
+openPicsFolder = function() {
+$dialog.dialog('open');
+}
+
+function openTagFolder(tag) {
+$("#" + tag + "-folder").on( 'click' , function() {
+$("." + tag + "").dialog('open')
+});
+}
+
+}
